8169e4fdb13e0af9723873c714f8fc1f67bf7001
fonca <eduard.cantos@gmail.com>
2025-12-24 21:42:19 +0100

    chore: initialize exercise project structure

---
+++ Level1/Exercise3/capitalGame-s1-t1-l1-e3/src/capitalgame/sp1ta1le1ex3/CapitalGame.java
@@ -0,0 +1,5 @@
+package capitalgame.sp1ta1le1ex3;
+
+public class CapitalGame {
+    //
+}
---
+++ Level1/Exercise3/capitalGame-s1-t1-l1-e3/src/capitalgame/sp1ta1le1ex3/Main.java
@@ -0,0 +1,7 @@
+package capitalgame.sp1ta1le1ex3;
+
+public class Main {
+    public static void main(String[] args) {
+        //
+    }
+}
